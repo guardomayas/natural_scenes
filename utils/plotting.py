ea3a7b38a29f53a21701a473dea4cf67c2de9bd9
Santiago <santigmaya@gmail.com>
2026-08-19 15:58:28 -0500

    more organization

--- utils/plotting.py
+++ utils/plotting.py
@@ -25,3 +25,27 @@ def show_hdr(img, ax=None, mode="log", lo=1, hi=99.5, title=None,
         ax.set_title(title, fontsize=8)
     ax.set_axis_off()
     return ax
+
+def plot_examples(imgs, n_rows, n_cols, random_seed=0):
+    n_samples = n_rows * n_cols
+    rng = np.random.default_rng(random_seed)
+    idx = rng.choice(len(imgs), size=n_samples, replace=False)
+    
+    fig, axes = plt.subplots(
+        n_rows, 
+        n_cols, 
+        figsize=(3 * n_cols, 3 * n_rows)
+    )
+
+    axes = np.atleast_2d(axes)
+
+    for ax, i in zip(axes.flat[:n_samples], idx):
+        img = imgs[i, :, :]
+        show_hdr(img, ax=ax)
+
+    for ax in axes.flat[n_samples:]:
+        ax.axis("off")
+
+    plt.tight_layout()
+    plt.show()
+    return fig, axes
